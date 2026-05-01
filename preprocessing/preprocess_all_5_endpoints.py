67d5ec0750aa1aef6670e498d2ae7546a8f40bc5
Zachary Smith <zsmith@bhsai.org>
2026-05-01 11:17:39 -0400

    Add readme instructions

--- preprocessing/preprocess_all_5_endpoints.py
+++ preprocessing/preprocess_all_5_endpoints.py
@@ -13,8 +13,8 @@ def main():
     for featurization in preprocess_compounds.featurizers.keys():
         for dataset in DATASETS:
             preprocess_compounds.preprocess_compounds(
-                raw_data_csv=f"../data/raw/{dataset}.csv",
-                output_dir=f"../data/preprocessed/{dataset}",
+                raw_data_csv=f"./data/raw/{dataset}.csv",
+                output_dir=f"./data/preprocessed/{dataset}",
                 class_col="Property",
                 featurization=featurization,
             )
